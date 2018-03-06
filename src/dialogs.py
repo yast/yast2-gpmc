cef81e82d563a21f235e7ce194d5ffe76bb2fc3d
David Mulder <dmulder@suse.com>
2018-03-06 16:23:29 -0700

    Still work under python2, and don't die if not joined

--- src/dialogs.py
+++ src/dialogs.py
@@ -266,12 +266,12 @@ class GPMC:
             try:
                 self.q = GPConnection(lp, creds)
                 self.gpos = self.q.gpo_list()
+                self.realm_dn = self.q.realm_to_dn(self.realm)
                 break
             except Exception as e:
                 print(str(e))
                 creds.set_password('')
                 self.got_creds = self.__get_creds(creds)
-        self.realm_dn = self.q.realm_to_dn(self.realm)
 
     def __setup_menus(self):
         UI.WizardCommand(Term('DeleteMenus'))
